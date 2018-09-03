d42928c517bf09d91726644466f0ac580cf0a4d1
AlexandreLadeira <32870665+AlexandreLadeira@users.noreply.github.com>
2018-09-03 11:43:52 -0300

    Scanner de porta remoto

--- ExercicioUDP/src/exercicioudp/ScannerDePortasRemoto.java
+++ ExercicioUDP/src/exercicioudp/ScannerDePortasRemoto.java
@@ -5,10 +5,47 @@
  */
 package exercicioudp;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  *
  * @author u16159
  */
 public class ScannerDePortasRemoto {
+     public static void main(String[] args) {
+       try {
+           byte[] dado = new byte[8192];
+           System.out.print("Digite o IP:");
+           
+           BufferedReader leitor = new BufferedReader(
+                                   new InputStreamReader(System.in)); 
+           
+           String ip = leitor.readLine();
+           
+           InetAddress endereco  = InetAddress.getByName(ip);
+           DatagramPacket pacote = new DatagramPacket(dado,8192);
+           
+           //i == porta que será testada 
+           for(int i = 1024; i <= 65535; i++) 
+               try {
+                 DatagramSocket servidor = new DatagramSocket(i);
+                 pacote.setAddress(endereco);
+                 servidor.send(pacote);
+                 
+                 System.out.println("A porta "+i+" está livre");
+                 servidor.close();
+               } catch(Exception e ) {
+                   System.out.println("Porta-"+i+"-em uso");
+               }
+              
+           
+       } catch (Exception e ) {
+           System.out.println("Oocorreu um erro:"+e.getMessage());
+       }
+    }
     
 }
